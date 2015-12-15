1f1f21a496e350cc0501410f657d6e08c672a80f
Brad Fisher <bradallenfisher@gmail.com>
2015-12-14 20:44:06 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#for RHEL type OS.
+#install node
+
+curl --silent --location https://rpm.nodesource.com/setup | bash -
+yum -y install nodejs
+yum -y install gcc-c++ make
+yum -y groupinstall 'Development Tools'
+
+#Example Chat project
+git clone https://github.com/rauchg/chat-example.git
+
+cd chat-example
+npm install --save express
+npm install --save socket.io
